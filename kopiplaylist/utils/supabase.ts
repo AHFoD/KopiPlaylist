292c3bb30cc5c10199f8705944c9751aed4d3f57
AliZulfaqar <muhdalizulfaqar@gmail.com>
2024-02-11 10:55:44 +0800

    refactor: remove php content

--- kopiplaylist/utils/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://hrpenrebklvfwhbvtmcm.supabase.co";
-const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhycGVucmVia2x2ZndoYnZ0bWNtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNDUyNDQxMCwiZXhwIjoyMDIwMTAwNDEwfQ.W8-6Uk-FFJPcIRPcOOH7fqEP828a9bVp70JpWqxrxZs";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
